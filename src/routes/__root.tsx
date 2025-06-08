fe6b7be042487f9b35e1e006cc9c5f8463ac6a25
fillip1984 <fillip1984@gmail.com>
2025-06-08 11:09:18 -0400

    added favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -32,7 +32,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Paratus TanStack Start Beta',
       },
     ],
     links: [
@@ -40,6 +40,24 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
     ],
   }),
 
