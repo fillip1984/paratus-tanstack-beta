145830fe541a601441e4343bd324fd6744d3b974
fillip1984 <fillip1984@gmail.com>
2025-07-04 13:44:22 -0400

    fixed formatting of spinner

--- src/components/shared/LoadOrRetry.tsx
+++ src/components/shared/LoadOrRetry.tsx
@@ -10,24 +10,23 @@ export default function LoadOrRetry({
   retry: () => void
 }) {
   return (
-    <div className="flex items-center justify-center">
-      {isLoading && <FaCircleNotch className="animate-spin text-8xl" />}
+    <div className="flex items-center justify-center flex-1">
+      {isLoading && <FaCircleNotch className="animate-spin text-9xl" />}
 
       {isError && (
         <div>
-          <h2 className="flex items-center justify-center gap-2 uppercase">
+          <h2 className="flex mb-0 items-center justify-center gap-2 uppercase">
             <FaExclamationTriangle /> error
           </h2>
-          <div className="my-8">
-            <p>An occurred has occurred, would you like to try?</p>
-            <button
-              type="button"
-              onClick={retry}
-              className="mt-1 w-full rounded bg-slate-400 px-4 py-2 text-3xl"
-            >
-              Retry
-            </button>
-          </div>
+          <p>An error has occurred, would you like to retry?</p>
+
+          <button
+            type="button"
+            onClick={retry}
+            className="mt-4 w-full rounded bg-slate-400 px-4 py-2 text-3xl"
+          >
+            Retry
+          </button>
         </div>
       )}
     </div>
